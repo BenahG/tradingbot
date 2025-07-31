26871af18975f47182838566d6930ab6d4038961
Benah <gichinibenard1@gmail.com>
2025-07-31 23:08:19 +0300

    Setup environment variables and prepare for deployment

--- tradingbot/settings.py
+++ tradingbot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ AUTH_USER_MODEL = 'users.User'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@(nks7g4ao!%5jft8%^jfpj4p$&t4l=xh862rmrlw7ku8m%iai"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
